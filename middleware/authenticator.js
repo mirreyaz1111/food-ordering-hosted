247224b8fb51710e8ce7538f17dedb899934cc0f
Danish <danishnazir2001@gmail.com>
2023-04-01 23:54:08 +0530

    authorization changed

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -1,25 +1,25 @@
-const jwt = require('jsonwebtoken');
-const {jwtSecret} = require('../config/keys');
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/keys");
 
 exports.authenticateJWT = (req, res, next) => {
-    const token = req.cookies.token;
-   
-    if(!token) {
-        return res.status(401).json({
-            errorMessage: "No token. Authentication denied",
-        })
-    }
+  const token = req.cookies.token || req.headers.authorization;
 
-    try{
-        const decoded =jwt.verify(token,jwtSecret);
-        console.log(decoded);
-        req.user=decoded.user
+  if (!token) {
+    return res.status(401).json({
+      errorMessage: "No token. Authentication denied",
+    });
+  }
 
-        next();
-    }catch(err){
-        console.log('jwt error: ', err);
-        res.status(401).json({
-            errorMessage: "Invalid token"
-        });
-    }
-}
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    console.log(decoded);
+    req.user = decoded.user;
+
+    next();
+  } catch (err) {
+    console.log("jwt error: ", err);
+    res.status(401).json({
+      errorMessage: "Invalid token",
+    });
+  }
+};
--- routes/food.js
+++ routes/food.js
@@ -1,15 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/product');
-const { authenticateJWT } = require('../middleware/authenticator');
-const upload = require('../middleware/multer');
+const productController = require("../controllers/product");
+const { authenticateJWT } = require("../middleware/authenticator");
+const upload = require("../middleware/multer");
 
-router.post("/", authenticateJWT, upload.single('productImage'), productController.create);
-
+router.post(
+  "/",
+  authenticateJWT,
+  upload.single("productImage"),
+  productController.create
+);
 
 router.get("/", productController.readAll);
 router.get("/:productId", productController.read);
-router.put("/:productId",authenticateJWT, upload.single('productImage'), productController.update);
+router.put(
+  "/:productId",
+  authenticateJWT,
+  upload.single("productImage"),
+  productController.update
+);
 router.delete("/:productId", authenticateJWT, productController.delete);
 
 module.exports = router;
